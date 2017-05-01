0d518e6f2c5016a83e15113f6af6d1fbbc57a8f1
rebelin <rlin2018@pingry.org>
2017-05-01 12:50:16 -0400

    Added tests.

---
+++ tests.py
@@ -0,0 +1,10 @@
+import palindrome
+
+print(palindrome.is_palindrome("Hannah"))
+print(palindrome.is_palindrome("racecar"))
+print(palindrome.is_palindrome("kayak"))
+print(palindrome.is_palindrome("no lemon no melon"))
+print(palindrome.is_palindrome(" "))
+print(palindrome.is_palindrome(" refer"))
+print(palindrome.is_palindrome("Red rum, sir, is murder"))
+print(palindrome.is_palindrome("Eva, can I see bees in a cave?"))
