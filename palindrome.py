c36d9163961be41ec153bf8aa0b7e72229ccfbb7
Abigail Berger <aberger2019@pingry.org>
2017-05-01 12:30:52 -0400

    is_palindrome function

---
+++ palindrome.py
@@ -0,0 +1,16 @@
+def is_palindrome(text) :
+	""" Takes in a string and determines if palindrome. Returns true or false. """
+	print(text)
+	
+	if len(text) == 1 :
+		return True
+		
+	elif len(text) == 2 and text[0] == text[-1] :
+		return True
+		
+	elif text[0] != text[-1] :
+		return False
+	
+	elif text[0] == text[-1] :
+		is_palindrome(text[1:-1])
+		return True
